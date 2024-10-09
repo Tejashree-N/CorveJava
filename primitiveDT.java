95072ba5f2a6eb5a5c588dcad6cef39afa176453
Tejashree-N <158763140+Tejashree-N@users.noreply.github.com>
2024-10-09 13:07:13 +0530

    Add files via upload

---
+++ primitiveDT.java
@@ -0,0 +1,18 @@
+package datatypes;
+
+public class primitiveDT {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		boolean flag=true;
+		System.out.println(flag);
+		
+		byte num1=14;
+		
+		System.out.println(flag);
+		long num2=1312546751;
+		
+
+	}
+
+}
