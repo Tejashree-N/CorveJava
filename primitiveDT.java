f0616acbb98d7e81aad82795b5b8985306609bf6
Tejashree-N <158763140+Tejashree-N@users.noreply.github.com>
2024-10-09 13:14:03 +0530

    Add files via upload

--- primitiveDT.java
+++ primitiveDT.java
@@ -3,16 +3,45 @@ package datatypes;
 public class primitiveDT {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		boolean flag=true;
-		System.out.println(flag);
 		
-		byte num1=14;
-		
-		System.out.println(flag);
-		long num2=1312546751;
-		
+		 // 2. short (2 bytes)
+        short var1 = 32000;
+
+        // 3. int (4 bytes)
+        int var2 = 100000;
+
+        // 4. long (8 bytes)
+        long var3  = 10000000000L;
+
+        // 5. char (2 bytes)
+        char var4 = 'A';
+
+        // 6. float (4 bytes)
+        float var5 = 3.145f;
+
+        // 7. double (8 bytes)
+        double var6 = 3.14159265359;
+
+        // 8. boolean (1 bit)
+        boolean var7 = false;
+
+        // Non-Primitive Data Types
+
+        // 1. String (non-primitive but often treated as a special class)
+        String str = "tejashree";
+        
+        
+        System.out.println(var1);
+        System.out.println(var2);
+        System.out.println(var3);
+        System.out.println(var4);
+        System.out.println(var5);
+        System.out.println(var6);
+        System.out.println(var7);
+        System.out.println(str);
+	}
+
 
 	}
 
-}
+
