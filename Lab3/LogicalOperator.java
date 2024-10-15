f3c16faa994ac445776ece86f14cbfd0fd8bebca
Tejashree-N <158763140+Tejashree-N@users.noreply.github.com>
2024-10-15 13:37:54 +0530

    Add files via upload

---
+++ Lab3/LogicalOperator.java
@@ -0,0 +1,33 @@
+package sample;
+
+public class LogicalOperator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int m=12;
+		int n=54;
+		if(m==n && n>m)
+		{
+			System.out.println("Result is true");
+		}
+		else
+		{
+			System.out.println("Result is false");
+		}
+
+		//OR
+		if(m==n || n>m)
+		{
+			System.out.println("Result is true");
+		}
+		else
+		{
+			System.out.println("Result is false");
+		}
+	}
+		
+
+	
+
+}
