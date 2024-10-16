aa8deb69665ce9a565871a10fda30cc650d516cb
Tejashree-N <158763140+Tejashree-N@users.noreply.github.com>
2024-10-16 12:59:03 +0530

    Add files via upload

---
+++ Lab4/InstanceVariable.java
@@ -0,0 +1,30 @@
+package variablesTypes;
+
+class Student
+{
+	
+	// Instance Var
+				String name="Nishitha";
+				double fees=1000;
+				public void show()
+				{
+				
+					System.out.println("This is an example of instance variable");
+				}
+}
+
+public class InstanceVariable {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Student obj=new Student();//Constructor
+		System.out.println(obj.name);
+		obj.fees=2000;
+		System.out.println("Student fees is:"+obj.fees);
+		
+		obj.show();
+		
+	}
+
+}
