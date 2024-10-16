5d1cd66cff7f8b6f8c9dfc0ef72577195d110656
Tejashree-N <158763140+Tejashree-N@users.noreply.github.com>
2024-10-16 23:38:03 +0530

    Add files via upload

---
+++ Lab4/LocalVariables.java
@@ -0,0 +1,45 @@
+package variablesTypes;
+
+class Subject
+{
+	//instance var
+	int English;
+	int Maths;
+	int GK;
+	public void dispaly()
+	{
+		//local var
+		int avg=60;
+		System.out.println(avg);
+	}
+
+}
+
+
+public class LocalVariables {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//Mark of Student1
+				Subject s1=new Subject();
+				//s1.English=80; bcs its visible only in that block only
+				s1.Maths=70;
+				s1.GK=90;
+				s1.English=80;
+				//System.out.println(s1.avg);we cant access local variable
+				
+				//Mark of Student2
+						Subject s2=new Subject();
+						s2.Maths=90;
+						s2.GK=80;
+						s2.English=70;
+						s1.dispaly();
+						
+				
+				System.out.println("English:"+s1.English+" "+"Maths:"+s1.Maths+" "+"gk:"+s1.GK);
+				System.out.println("English:"+s2.English+" "+"Maths:"+s2.Maths+" "+"gk:"+s2.GK);
+
+	}
+
+}
