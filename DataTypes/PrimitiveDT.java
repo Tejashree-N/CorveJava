c4b9becaba46f505c29d98fcafe5651227cf611a
Tejashree-N <158763140+Tejashree-N@users.noreply.github.com>
2024-11-07 16:23:54 +0530

    Add files via upload

---
+++ DataTypes/PrimitiveDT.java
@@ -0,0 +1,44 @@
+package datatypes;
+
+public class PrimitiveDT {
+	public static void main(String[] args) {
+		
+		 // 2. short (2 bytes)
+       short Datatype1 = 1200;
+
+       // 3. int (4 bytes)
+       int Datatype2 = 10;
+
+       // 4. long (8 bytes)
+       long Datatype3  = 10000000000L;
+
+       // 5. char (2 bytes)
+       char Datatype4 = 'A';
+
+       // 6. float (4 bytes)
+       float Datatype5 = 5.555f;
+
+       // 7. double (8 bytes)
+       double Datatype6 = 3.785559;
+
+       // 8. boolean (1 bit)
+       boolean Datatype7 = true;
+
+       // Non-Primitive Data Types
+
+       // 1. String (non-primitive but often treated as a special class)
+       String str = "tejashree";
+       
+       
+       System.out.println(Datatype1);
+       System.out.println(Datatype2);
+       System.out.println(Datatype3);
+       System.out.println(Datatype4);
+       System.out.println(Datatype5);
+       System.out.println(Datatype6);
+       System.out.println(Datatype7);
+       System.out.println(str);
+	}
+
+
+}
