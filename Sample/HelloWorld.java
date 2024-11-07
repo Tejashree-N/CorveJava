6356758436998a51ac58b1eee357a32ab8d0776e
Tejashree-N <158763140+Tejashree-N@users.noreply.github.com>
2024-11-07 16:18:16 +0530

    Add files via upload

---
+++ Sample/HelloWorld.java
@@ -0,0 +1,17 @@
+package sample;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+			int number = 10;
+			System.out.println("Hello World");
+			System.out.println("my number is: "+ number);
+
+		
+
+	}
+
+}
